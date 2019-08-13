5c5985d84dd7afba35c719ab7dd89037c8045511
Magdiel <magdiel.palacios@csfacturacion.com>
2019-08-12 22:43:50 -0500

    Agregando controlador Cities

---
+++ CityInfo/CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace CityInfo.Controllers
+{
+    public class CitiesController : Controller
+    {
+
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new { Id = 1, Name = ""},
+                new {Id = 2, Name = ""}
+            });
+        }
+    }
+}
